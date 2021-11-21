30dba0ae48ba27482728cd3a3950014260ea645e
joao-pm-santos96 <joao.pm.santos96@gmail.com>
2021-11-21 14:39:11 +0000

    implemented class. something not working

--- ar_paint.py
+++ ar_paint.py
@@ -22,9 +22,240 @@ TODO
 """
 CLASS DEFINITIONS
 """
-# class ImageHandler:
-#     def __init__(self): # to avoid using global variables
-#         pass
+class ImageHandler:
+    def __init__(self, shake_prevention = False, mirror = False, camera_mode = False, paint_mode = False): # to avoid using global variables
+
+        self.shake_prevention = shake_prevention
+        self.mirror = mirror
+        self.camera_mode = camera_mode
+        self.paint_mode = paint_mode
+
+        self.pencil_lower_limits = None
+        self.pencil_upper_limits = None 
+
+        self.video_window = 'Video'
+        self.canvas_window = 'Canvas'
+        self.mask_window = 'Mask'
+        self.goal_paint_window = 'Goal Paint'
+
+        self.capture = None
+        self.img_width = None
+        self.img_height = None
+        self.n_channels = 4
+
+        self.canvas = None
+        self.persistent_background = None
+        self.goal_paint = None
+        self.centroid = None
+
+        self.pencil = {'size': 10, 'color': (0, 0, 255, 255), "last_point": None, "shape": '.'}
+
+        self.shake_threshold = 150
+        self.mouse_handler = MouseHandler(self.canvas, self.pencil)
+
+    def getLimitsFromFile(self, file):
+        try:
+            f = open(file)
+            limits = json.load(f)
+
+            # Get thresholds
+            self.pencil_lower_limits = np.array([limits['limits'][color]['min'] for color in 'bgr'])
+            self.pencil_upper_limits_limits = np.array([limits['limits'][color]['max'] for color in 'bgr'])
+
+        except:
+            print(f"Erro a ler o ficheiro {file}!")
+            exit(1)
+
+    def startWindows(self):
+
+        cv2.namedWindow(self.video_window)
+        cv2.namedWindow(self.mask_window)
+        cv2.namedWindow(self.canvas_window)
+
+    def setCanvasCallback(self):
+        cv2.setMouseCallback(self.canvas_window, self.mouse_handler.onMouseClick)
+
+    def startVideoCapture(self, index=0):
+
+        try:
+            self.capture = cv2.VideoCapture(index)
+
+            # Get initial frame and size
+            if self.capture.isOpened(): 
+                self.img_width  = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+                self.img_height = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            else:
+                raise Exception(f'Could not open camera {index}.')
+
+        except:
+            pass # TODO
+
+    def startCanvas(self):
+
+        # Create white canvas
+        self.canvas = np.zeros((self.img_height, self.img_width, self.n_channels), np.uint8)
+        self.persistent_background = np.zeros((self.img_height, self.img_width, self.n_channels), np.uint8)
+        
+        if not self.camera_mode:
+            self.canvas.fill(255)
+
+        try:
+            if self.paint_mode:
+
+                # Read the image to be painted and build a mask
+                self.goal_paint = cv2.imread(self.paint_mode, cv2.IMREAD_COLOR)
+                self.goal_paint = cv2.resize(self.goal_paint, (self.img_width, self.img_height))
+
+                # Build mask
+                lower = np.array([200 for color in 'bgr']) 
+                upper = np.array([256 for color in 'bgr']) 
+                mask = cv2.inRange(self.goal_paint, lower, upper)
+
+                # Get stats
+                num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
+                
+                # Paint each region randomly
+                self.goal_paint.fill(0)
+                region_color = [255,0,0]
+                for idx in range(1,num_labels):
+                    random.shuffle(region_color)
+                    self.goal_paint[labels==idx] = region_color
+
+                self.persistent_background[labels==0,3] = 255    
+        except:
+            print(f"Erro a ler o ficheiro {self.paint_mode}!")
+            exit(1)
+
+    def handleKey(self, key):
+        
+        # Check if drawing rectangle or circle
+        shape = chr(key) if key in [ord('s'), ord('e')] else None
+
+        if shape and self.pencil["last_point"]:
+            if shape == self.pencil["shape"]:
+                drawShape(self.canvas, self.pencil, self.centroid)
+                self.pencil["shape"] = '.'
+                self.pencil["last_point"] = None
+            else:
+                self.pencil["shape"] = shape
+                self.pencil["last_point"] = self.centroid
+
+        elif key == ord('r'):
+            if self.pencil['color'] != (0, 0, 255, 255):
+                self.pencil['color'] = (0, 0, 255, 255)
+                print('Set pencil color to ' + Fore.RED + 'red' + Style.RESET_ALL)
+
+        elif key == ord('g'):
+            if self.pencil['color'] != (0, 255, 0, 255):
+                self.pencil['color'] = (0, 255, 0, 255)
+                print('Set pencil color to ' + Fore.GREEN + 'green' + Style.RESET_ALL)
+        
+        elif key == ord('b'):
+            if self.pencil['color'] != (255, 0, 0, 255):
+                self.pencil['color'] = (255, 0, 0, 255)
+                print('Set pencil color to ' + Fore.BLUE + 'blue' + Style.RESET_ALL)
+
+        elif key == ord('+'):
+            self.pencil['size'] += 1
+            print('Increased pencil size to ' + str(self.pencil['size']))
+        
+        elif key == ord('-'):
+            if self.pencil['size'] > 2:
+                self.pencil['size'] -= 1
+                print('Decreased pencil size to ' + str(self.pencil['size']))
+            else:
+                print('Can not decrease any further')
+
+        elif key == ord('c'):
+            if self.camera_mode: # make all pixels transparent
+                self.canvas[:,:,:] = 0 
+            else: # paint all pixels as white
+                self.canvas.fill(255)
+            print('Cleared canvas')
+        
+        elif key == ord('w'):
+            file_name = 'drawing_' + datetime.now().strftime('%a_%b_%m_%H:%M:%S_%Y') + '.png'
+            cv2.imwrite(file_name, self.drawing)
+            print('Saved canvas to ' + file_name)
+
+        elif key == ord('q'):
+            return True  
+
+
+    def main(self):
+
+        # Loop
+        while True:
+
+            # Read frame
+            _, frame = self.capture.read()
+
+            # Mirror image for better compreension
+            if self.mirror:
+                frame = cv2.flip(frame, 1) # code for horizontal         
+
+            # Get mask of drawing tool
+            mask = cv2.inRange(frame, self.pencil_lower_limits, self.pencil_upper_limits)
+
+            # Get pointer
+            num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
+
+            # Get max area mask
+            if num_labels > 1:
+                
+                # Get index ignoring the first one
+                index = np.argmax(stats[1:, cv2.CC_STAT_AREA]) + 1
+
+                # Get mask and centroid
+                mask_max = (labels == index).astype('uint8') * 255
+                self.centroid = tuple(centroids[index, :].astype(np.int32))
+
+                # If not using mouse, draw the crosshair
+                if not self.mouse_handler.drawing:
+                    frame = drawCrosshair(frame, self.centroid, color=self.pencil['color'] )
+                
+                last_point = self.pencil["last_point"] # help legibility
+
+                # Compute norm between consecutive points
+                norm = distanceOf2Points(last_point, self.centroid) if (self.shake_prevention and last_point is not None) else 0
+
+                # Draw line
+                if self.pencil["shape"] == '.':
+                    if last_point is not None and norm < self.shake_threshold and not self.mouse_handler.drawing:
+                        cv2.line(self.canvas, last_point, self.centroid, self.pencil['color'], self.pencil['size'])
+
+                    # Save last point
+                    self.pencil["last_point"] = self.centroid
+                else:
+                    drawShape(self.canvas, self.pencil, self.centroid)
+
+                cv2.imshow(self.mask_window, mask_max)
+
+            # Combine frame with drawing
+            drawing = drawOnImage(frame, self.canvas) if self.camera_mode else self.canvas.copy()        
+            drawing = drawOnImage(drawing[:,:,:3], self.persistent_background)
+
+            if self.paint_mode:
+                # Give the painting a score
+                black_template = np.array([0,0,0]) 
+                valid_indexes = np.bitwise_not(np.all(self.goal_paint==black_template,axis=2))
+
+                all_hits = np.all(self.goal_paint==self.canvas[:,:,:3],axis=2)
+            
+                all_valid_hits = np.logical_and(all_hits, valid_indexes)
+            
+                goal_display = self.goal_paint.copy()
+
+                accuracy = all_valid_hits.sum()/valid_indexes.sum()
+                cv2.putText(goal_display,f"{accuracy*100:.2f}%",(10, goal_display.shape[0]-50), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,255,255),2,cv2.LINE_AA)
+                cv2.imshow(self.goal_paint_window, goal_display)
+
+            # Show 
+            cv2.imshow(self.video_window, frame)
+            cv2.imshow(self.canvas_window, drawing)
+
+            if self.handleKey(cv2.waitKey(1)):
+                break 
 
 
 class MouseHandler:
@@ -168,212 +399,31 @@ def main():
 
     args = parser.parse_args()
 
-    # Read file    
-    try:
-        f = open(args.json)
-        limits = json.load(f)
-    except:
-        print(f"Erro a ler o ficheiro {args.json}!")
-        exit(1)
-
     # Print welcome message
     welcomeMessage()
 
-    # Get thresholds
-    lower = np.array([limits['limits'][color]['min'] for color in 'bgr'])
-    upper = np.array([limits['limits'][color]['max'] for color in 'bgr'])
+    image_handler = ImageHandler(shake_prevention=args.use_shake_prevention, 
+    mirror=args.mirror,
+    camera_mode=args.cameramode,
+    paint_mode=args.paintmode)
 
-    # Init frame and VideoCapture
-    video_window = 'Video'
-    canvas_window = 'Canvas'
-    mask_window = 'Mask'
-    goal_paint_window = 'Goal Paint'
+    # Read file   
+    image_handler.getLimitsFromFile(args.json)     
 
-    cv2.namedWindow(video_window)
-    cv2.namedWindow(mask_window)
-    cv2.namedWindow(canvas_window)
+    image_handler.startWindows()
 
-    cap = cv2.VideoCapture(0)
+    image_handler.startVideoCapture()
     
-    # Get initial frame and size
-    if cap.isOpened(): 
-        width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    else:
-        raise Exception('Could not open camera 0.')
-    
-    # Create white canvas
-    n_channels = 4
-    canvas = np.zeros((height, width, n_channels), np.uint8)
-    persistent_background = np.zeros((height, width, n_channels), np.uint8)
-    
-    if not args.cameramode:
-        canvas.fill(255)
-
-    try:
-        if args.paintmode:
-
-            # Read the image to be painted and build a mask
-            goal_paint = cv2.imread(args.paintmode, cv2.IMREAD_COLOR)
-            goal_paint = cv2.resize(goal_paint, (width, height))
-
-            # Build mask
-            lower = np.array([200 for color in 'bgr']) 
-            upper = np.array([256 for color in 'bgr']) 
-            mask = cv2.inRange(goal_paint, lower, upper)
-
-            # Get stats
-            num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
-            
-            # Paint each region randomly
-            goal_paint.fill(0)
-            region_color = [255,0,0]
-            for idx in range(1,num_labels):
-                random.shuffle(region_color)
-                goal_paint[labels==idx] = region_color
-
-            persistent_background[labels==0,3] = 255
-   
-    except:
-        print(f"Erro a ler o ficheiro {args.paintmode}!")
-        exit(1)
-
-    # Pencil start state
-    pencil = {'size': 10, 'color': (0, 0, 255, 255), "last_point": None, "shape": '.'}    
+    image_handler.startCanvas()
 
     # Start mouse callback for drawing
-    mouse_handler = MouseHandler(canvas, pencil)
-    cv2.setMouseCallback(canvas_window, mouse_handler.onMouseClick)
+    image_handler.setCanvasCallback()
 
-    # Misc initializations
-    norm_threshold = 150
+    # image_handler.main()
 
-    # Loop
-    while True:
-
-        # Read frame
-        _, frame = cap.read()
-
-        # Mirror image for better compreension
-        if args.mirror:
-            frame = cv2.flip(frame, 1) # code for horizontal         
-
-        # Get mask of drawing tool
-        mask = cv2.inRange(frame, lower, upper)
-
-        # Get pointer
-        num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
-
-        # Get max area mask
-        if num_labels > 1:
-            
-            # Get index ignoring the first one
-            index = np.argmax(stats[1:, cv2.CC_STAT_AREA]) + 1
-
-            # Get mask and centroid
-            mask_max = (labels == index).astype('uint8') * 255
-            centroid = tuple(centroids[index, :].astype(np.int32))
-            # Draw cross
-
-            # If not using mouse, draw the crosshair
-            if not mouse_handler.drawing:
-                frame = drawCrosshair(frame, centroid, color=pencil['color'] )
-         
-            last_point = pencil["last_point"] # help legibility
-            
-            # Compute norm between consecutive points
-            norm = distanceOf2Points(last_point, centroid) if (args.use_shake_prevention and last_point is not None) else 0
-
-            # Draw line
-            if pencil["shape"] == '.':
-                if last_point is not None and norm < norm_threshold and not mouse_handler.drawing:
-                    cv2.line(canvas, last_point, centroid, pencil['color'], pencil['size'])
-
-                # Save last point
-                pencil["last_point"] = centroid
-            else:
-                drawShape(canvas, pencil, centroid)
-
-            cv2.imshow(mask_window, mask_max)
-
-        # Combine frame with drawing
-        drawing = drawOnImage(frame, canvas) if args.cameramode else canvas.copy()        
-        drawing = drawOnImage(drawing[:,:,:3], persistent_background)
-
-        if args.paintmode:
-            # Give a painting score
-            black_template = np.array([0,0,0]) 
-            valid_indexes = np.bitwise_not(np.all(goal_paint==black_template,axis=2))
-       
-            all_hits = np.all(goal_paint==canvas[:,:,:3],axis=2)
-            
-            all_valid_hits = np.logical_and(all_hits, valid_indexes)
-          
-            goal_display = goal_paint.copy()
-            
-            accuracy = all_valid_hits.sum()/valid_indexes.sum()
-            cv2.putText(goal_display,f"{accuracy*100:.2f}%",(10, goal_display.shape[0]-50), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,255,255),2,cv2.LINE_AA)
-            cv2.imshow(goal_paint_window, goal_display)
-
-        # Show 
-        cv2.imshow(video_window, frame)
-        cv2.imshow(canvas_window, drawing)
         
-        # Key controls
-        key = cv2.waitKey(1)
 
-        # Check if drawing rectangle or circle
-        shape = chr(key) if key in [ord('s'), ord('e')] else None
-
-        if shape and pencil["last_point"]:
-            if shape == pencil["shape"]:
-                drawShape(canvas, pencil, centroid)
-                pencil["shape"] = '.'
-                pencil["last_point"] = None
-            else:
-                pencil["shape"] = shape
-                pencil["last_point"] = centroid
-
-        elif key == ord('r'):
-            if pencil['color'] != (0, 0, 255, 255):
-                pencil['color'] = (0, 0, 255, 255)
-                print('Set pencil color to ' + Fore.RED + 'red' + Style.RESET_ALL)
-
-        elif key == ord('g'):
-            if pencil['color'] != (0, 255, 0, 255):
-                pencil['color'] = (0, 255, 0, 255)
-                print('Set pencil color to ' + Fore.GREEN + 'green' + Style.RESET_ALL)
         
-        elif key == ord('b'):
-            if pencil['color'] != (255, 0, 0, 255):
-                pencil['color'] = (255, 0, 0, 255)
-                print('Set pencil color to ' + Fore.BLUE + 'blue' + Style.RESET_ALL)
-
-        elif key == ord('+'):
-            pencil['size'] += 1
-            print('Increased pencil size to ' + str(pencil['size']))
-        
-        elif key == ord('-'):
-            if pencil['size'] > 2:
-                pencil['size'] -= 1
-                print('Decreased pencil size to ' + str(pencil['size']))
-            else:
-                print('Can not decrease any further')
-
-        elif key == ord('c'):
-            if args.cameramode: # make all pixels transparent
-                canvas[:,:,:] = 0 
-            else: # paint all pixels as white
-                canvas.fill(255)
-            print('Cleared canvas')
-        
-        elif key == ord('w'):
-            file_name = 'drawing_' + datetime.now().strftime('%a_%b_%m_%H:%M:%S_%Y') + '.png'
-            cv2.imwrite(file_name, drawing)
-            print('Saved canvas to ' + file_name)
-
-        elif key == ord('q'):
-            break  
             
 """
 MAIN
